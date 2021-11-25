7d38a3653844cb7b5056053cc130354ca81cb0b0
Muri Budiman <udamuri@gmail.com>
2021-11-25 21:20:00 +0700

    respati training API CRUD View

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Models\Post;
 
@@ -13,21 +14,33 @@ class PostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $result = Post::all();
+        try {
+            $limit = $request->has('limit') ? $request->limit : 25;
 
-        return response()->json($result);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        
+            $model = Post::applyFilters($request->only([
+                    'search',
+                ]))
+                ->latest()
+                ->paginateData($limit);
+            
+            $paginate = [
+                'total' => (int) $model->total(),
+                'currentPage' => (int) $model->currentPage(),
+                'lastPage' => (int) $model->lastPage(),
+                'hasMorePages' => (boolean) $model->hasMorePages(),
+                'perPage' => (int) $model->perPage(),
+                'total' => (int) $model->total(),
+                'lastItem' => (int) $model->lastItem(),
+            ];
+            
+            $result = api_format(true, [], $model->items(), $paginate);
+            return response()->json($result);
+        } catch (\Throwable $e) {
+            $result = api_format(false, $e, [], []);
+            return response()->json($result);
+        }
     }
 
     /**
@@ -38,29 +51,30 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        try {
+            $validator = Validator::make($request->all(), [
+                "title" => "required|max:255",
+                "body" => "required",
+            ]);
+            
+            $validator->setAttributeNames([
+                'title' => 'Title',
+                'body' => 'Body',
+            ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+            if ($validator->fails()) {
+                $result = api_format(false, $validator->errors()->toArray(), [], []);
+                return response()->json($result);
+            } else {
+                $create = Post::createModel($request);
+                $result = api_format(true, [], [$create], []);
+                return response()->json($result);
+            }
+        } catch (\Throwable $e) {
+            $result = api_format(false, $e, [], []);
+            return response()->json($result);
+        }
+        
     }
 
     /**
@@ -70,9 +84,31 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                "title" => "required|max:255",
+                "body" => "required",
+            ]);
+            
+            $validator->setAttributeNames([
+                'title' => 'Title',
+                'body' => 'Body',
+            ]);
+
+            if ($validator->fails()) {
+                $result = api_format(false, $validator->errors()->toArray(), [], []);
+                return response()->json($result);
+            } else {
+                $update = $post->updateModel($request);
+                $result = api_format(true, [], [$update], []);
+                return response()->json($result);
+            }
+        } catch (\Throwable $e) {
+            $result = api_format(false, $e, [], []);
+            return response()->json($result);
+        }
     }
 
     /**
@@ -81,8 +117,14 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        try {
+            $post->delete();
+            $result = api_format(true, ["Delet Posts"], [], []);
+        } catch (\Throwable $e) {
+            $result = api_format(false, $e, [], []);
+            return response()->json($result);
+        }
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,4 +13,54 @@ class Post extends Model
         'title',
         'body'
     ];
+
+    public $timestamps = true;
+
+    public function scopeWhereSearch($query, $search)
+    {
+        foreach (explode(' ', $search) as $term) {
+            $query->where('posts.title', 'LIKE', '%'.$term.'%')
+            ->orWhere('posts.body', 'LIKE', '%'.$term.'%');
+        }
+    }
+
+    public function scopeApplyFilters($query, array $filters)
+    {
+        $filters = collect($filters);
+        if ($filters->get('search')) {
+            $query->whereSearch($filters->get('search'));
+        }
+    }
+
+    public function scopePaginateData($query, $limit)
+    {
+        if ($limit == 'all') {
+            return collect(['data' => $query->get()]);
+        }
+
+        return $query->paginate($limit);
+    }
+
+    public static function createModel($request) {
+        $data = $request->only([
+            'title',
+            'body'
+        ]);
+        
+        $result = self::create($data);
+
+        return $result;
+    }
+
+    public function updateModel($request) {
+        $data = $request->only([
+            'title',
+            'body'
+        ]);
+        
+        $this->update($data);
+
+        $result = self::find($this->id);
+        return $result;
+    }
 }
